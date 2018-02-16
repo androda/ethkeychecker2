ca4986677ad4ef3739847fe3fa2dbc809a12c3ed
Tyler Tolley <tyler.tolley@domo.com>
2018-02-16 13:21:50 -0700

    Add background and separator

--- src/main/java/com/lostphoenix/SvgGenerator.java
+++ src/main/java/com/lostphoenix/SvgGenerator.java
@@ -13,8 +13,8 @@ import java.util.Set;
 public class SvgGenerator {
 
     static Set<SegmentType> mostFrequentSegments = Sets.newHashSet(
-            SegmentType.LIGHT_GREEN_M,
-            SegmentType.DARK_GREEN_L,
+//            SegmentType.LIGHT_GREEN_M,
+//            SegmentType.DARK_GREEN_L
             SegmentType.BLUE_S,
             SegmentType.BLUE_XL,
             SegmentType.LIGHT_GREEN_XL,
@@ -25,21 +25,19 @@ public class SvgGenerator {
     public static void main(String[] args) {
 
         try (PrintWriter out = new PrintWriter(new FileWriter("generated.svg"))){
-            out.println("<svg id=\"svg\" width=\"800\" height=\"800\" xmlns=\"http://www.w3.org/2000/svg\">");
+            out.println("<svg id=\"svg\" width=\"850\" height=\"850\" xmlns=\"http://www.w3.org/2000/svg\">");
+            out.println("<rect x=\"0\" y=\"0\" width=\"850\" height=\"850\"/>");
             for (int ring = 0; ring < 16; ring++) {
                 for (int segment = 0; segment < 4; segment++) {
-//                System.out.println("ring:"+ring+" segment:"+segment+" xStartOffset:"+xStartOffset(ring,segment)+" yStartOffset:"+yStartOffset(ring, segment)+" xEndPosition:"+xEndPosition(ring,segment)+" yEndPosition:"+yEndPosition(ring, segment));
                     SegmentType segmentType = Ring.segmentOrder[ring * 4 + segment];
-                    if (segmentType.color.equals(Color.DG))
-                        out.println("<path d=\"" + getDAttribute(ring, segment) + "\" style=\"stroke: " + colorString.get(segmentType.color) + "; stroke-width: " + thicknessString.get(segmentType.thickness) + "; fill:none;\"></path>");
+                    out.println("<path d=\""+getDAttributeWhiteSpaceTop(ring, segment)+"\" style=\"stroke: rgb(64,64,64) ; stroke-width: 5; fill:none;\"></path>");
+                    out.println("<path d=\""+getDAttributeWhiteSpaceBottom(ring, segment)+"\" style=\"stroke: rgb(64,64,64) ; stroke-width: 5; fill:none;\"></path>");
+                    if(mostFrequentSegments.contains(segmentType))
+
+                    out.println("<path d=\"" + getDAttribute(ring, segment) + "\" style=\"stroke: " + colorString.get(segmentType.color) + "; stroke-width: " + thicknessString.get(segmentType.thickness) + "; fill:none;\"></path>");
+
 
                 }
-//            for(int segment =1; segment<F2;segment++)
-//            {
-//                SegmentType segmentType = Application2.segmentOrder[ring*4+segment];
-//                System.out.println("<path d=\"M 0 "+(4+((ring+16)*24))+" l 97 0 \" style=\"stroke: rgb(0,0,0)"/*+colorString.get(segmentType.color)*/+"; stroke-width: "+thicknessString.get(segmentType.thickness)+"; fill:none;\"></path>"   );
-//
-//            }
             }
             out.println("</svg>");
 
@@ -49,23 +47,32 @@ public class SvgGenerator {
     }
 
     public static String getDAttribute(int ring, int segment) {
-        return String.format("M %s %s A %s %s 0 0 0 %s %s", xStartOffset(ring, segment), yStartOffset(ring, segment), 20 + ring * 24, 20 + ring * 24, xEndPosition(ring, segment), yEndPosition(ring, segment));
+        return String.format("M %s %s A %s %s 0 0 0 %s %s", xStartOffset(ring, segment), yStartOffset(ring, segment), 25 + ring * 25, 25 + ring * 25, xEndPosition(ring, segment), yEndPosition(ring, segment));
     }
 
-    public static double xStartOffset(int ring, int segment) {
-        return 400 + ((20 + (24 * ring)) * Math.cos((ring + (4 * segment)) * (Math.PI / 8.0)));
+    public static String getDAttributeWhiteSpaceTop(int ring, int segment) {
+        return String.format("M %s %s A %s %s 0 0 0 %s %s", (425+(38+25*ring)), 425, 25 + ring * 25, 25 + ring * 25, (425-(38+25*ring)), 425);
+
+    }
+
+    public static String getDAttributeWhiteSpaceBottom(int ring, int segment) {
+        return String.format("M %s %s A %s %s 0 0 0 %s %s", (425-(38+25*ring)), 425, 25 + ring * 25, 25 + ring * 25, (425+(38+25*ring)), 425);
+
+    }
+        public static double xStartOffset(int ring, int segment) {
+        return 425 + ((25 + (25 * ring)) * Math.cos((ring + (4 * segment)) * (Math.PI / 8.0)));
     }
 
     public static double yStartOffset(int ring, int segment) {
-        return 400 + ((20 + (24 * ring)) * Math.sin((ring + (4 * segment)) * (Math.PI / 8.0)));
+        return 425 + ((25 + (25 * ring)) * Math.sin((ring + (4 * segment)) * (Math.PI / 8.0)));
     }
 
     public static double xEndPosition(int ring, int segment) {
-        return 400 + ((20 + (24 * ring)) * Math.cos((ring + (4 * segment) - 4) * (Math.PI / 8.0)));
+        return 425 + ((25 + (25 * ring)) * Math.cos((ring + (4 * segment) - 4) * (Math.PI / 8.0)));
     }
 
     public static double yEndPosition(int ring, int segment) {
-        return 400 + ((20 + (24 * ring)) * Math.sin((ring + (4 * segment) - 4) * (Math.PI / 8.0)));
+        return 425 + ((25 + (25 * ring)) * Math.sin((ring + (4 * segment) - 4) * (Math.PI / 8.0)));
     }
 
     static Map<Color, String> colorString = ImmutableMap.of(Color.DG, "rgb(51, 85, 51)", Color.LG, "rgb(102, 136, 68)", Color.BLUE, "rgb(68, 102, 136)", Color.PINK, "rgb(136, 68, 102)");
